bd464c3a0039d6980b8b1911532eb84463918e81
EZU <gioc@example.com>
2023-04-18 04:34:11 +0400

    Add project files.

---
+++ JwtWebApiTutorial/Models/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace JwtWebApiTutorial.Models
+{
+    public class UserDto
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string UserPassword { get; set; } = string.Empty;
+    }
+}
